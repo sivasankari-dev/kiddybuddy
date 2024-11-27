174c031dd9dd7833e76d8b56268e5206ab733c73
sivasankari-dev <sivasankari.devofficial@gmail.com>
2024-11-27 13:58:19 +0530

    Reorganizing App structure

--- src/App.js
+++ src/App.js
@@ -6,22 +6,6 @@ import ToyCard from './components/ToyCard';
 function App() {
   return (
     <div className="App">
-        {/* <nav>
-        <h1 className='brandname'>Kidzone</h1>
-        <div className='headerlist'>
-        <ul>
-          <li>
-          <i className="fas fa-user fa-lg icon"></i> 
-          </li>
-          <li>
-          <i class="fas fa-shopping-cart fa-lg icon"></i>
-          </li>
-          <li>
-          <i class="fas fa-heart fa-lg icon"></i>
-          </li>
-        </ul>
-        </div>
-       </nav>  */}
        <div className='Header'>
           <NavBar/>
        </div>
